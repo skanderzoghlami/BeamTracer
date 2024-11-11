6abfdd36fe108e67414ca89428e269019f65d513
unknown <skander.zoghlami@etu.univ-lyon1.fr>
2024-11-11 22:03:12 +0100

    First commit, imgui starter

---
+++ main.cpp
@@ -0,0 +1,78 @@
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
+#include <iostream>
+
+
+
+void ppm_image_writer(const int image_width= 250 , const int image_height=250 , const char *filename)
+{
+    if(image_width <= 0 || image_height <= 0)
+    {
+        std::cerr << "Invalid image size" << std::endl;
+        return;
+    }
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+    if (!glfwInit()) { return -1; }
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "ImGui Text Input", NULL, NULL);
+    if (!window) { glfwTerminate(); return -1; }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    char inputText[128] = "";  
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Begin("Console Output Example");
+        ImGui::InputText("Input", inputText, IM_ARRAYSIZE(inputText));
+        if (ImGui::Button("Submit")) {
+            std::cout << "Submitted Text: " << inputText << std::endl;
+        }
+        ImGui::End();
+
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
