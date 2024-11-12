4bc244a0e719af4dd6b017881e81964101382ac1
unknown <skander.zoghlami@etu.univ-lyon1.fr>
2024-11-12 12:03:54 +0100

    add the write ppm function

---
+++ README.md
@@ -0,0 +1,2 @@
+# We just started !
+Current image : 
--- main.cpp
+++ main.cpp
@@ -6,14 +6,32 @@
 
 
 
-void ppm_image_writer(const int image_width= 250 , const int image_height=250 , const char *filename)
+void ppm_image_writer(const int image_width , const int image_height , const char *filename)
 {
     if(image_width <= 0 || image_height <= 0)
     {
         std::cerr << "Invalid image size" << std::endl;
         return;
     }
-    
+    FILE *fp = fopen(filename, "wb");
+    if (!fp) {
+        std::cerr << "Unable to open file." << std::endl;
+        return;
+    }
+    fprintf(fp, "P3\n%d %d\n255\n", image_width, image_height);
+    for (int j = image_height-1; j >= 0; --j) {
+        for (int i = 0; i < image_width; ++i) {
+            auto r = double(i) / (image_width-1);
+            auto g = double(j) / (image_height-1);
+            auto b = 0.25;
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+            fprintf(fp, "%d %d %d\n", ir, ig, ib);
+        }
+    }
+    fclose(fp);
+    std::cout << "Image written to " << filename << std::endl;
 
 }
 
@@ -30,6 +48,7 @@ void ppm_image_writer(const int image_width= 250 , const int image_height=250 , 
 
 
 int main() {
+
     if (!glfwInit()) { return -1; }
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "ImGui Text Input", NULL, NULL);
@@ -44,7 +63,8 @@ int main() {
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 130");
 
-    char inputText[128] = "";  
+    char inputText[256] = "250";
+    char inputText2[256] = "250";
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
@@ -53,10 +73,11 @@ int main() {
         ImGui::NewFrame();
 
         ImGui::Begin("Console Output Example");
-        ImGui::InputText("Input", inputText, IM_ARRAYSIZE(inputText));
+        ImGui::InputText("Image Width", inputText, IM_ARRAYSIZE(inputText));
+        ImGui::InputText("ImageHeight", inputText2, IM_ARRAYSIZE(inputText2));
         if (ImGui::Button("Submit")) {
             std::cout << "Submitted Text: " << inputText << std::endl;
-        }
+            ppm_image_writer(std::stoi(inputText), std::stoi(inputText2), "output.ppm");}
         ImGui::End();
 
         ImGui::Render();
