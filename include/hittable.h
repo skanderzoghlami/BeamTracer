51a18fe5e1673a1a782bc7c7c3fb4c87bec28287
unknown <skander.zoghlami@etu.univ-lyon1.fr>
2025-03-14 18:19:37 +0100

    Task: Add hittable objects and hittable lists

---
+++ include/hittable.h
@@ -0,0 +1,25 @@
+#ifndef HITTABLE_H
+#define HITTABLE_H
+#include "ray.h"
+#include "vec3.h"
+#include "color.h"
+
+class hit_record{
+    public:
+        point3 p;
+        double t;
+        vec3 normal;
+        bool front_face;
+        void set_front_face(const ray& r, const vec3& outward_normal){
+            front_face = dot(r.get_direction(),outward_normal) < 0 ;
+            normal = front_face ? outward_normal : -outward_normal;
+        }
+};
+
+class hittable{
+    public:
+    virtual bool hit(const ray&r , double t_min , double t_max , hit_record& rec) const = 0 ;
+    virtual ~hittable() = default;
+};
+
+#endif // HITTABLE_H
---
+++ include/sphere.h
@@ -0,0 +1,38 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.h"
+#include "vec3.h"
+#include "ray.h"
+
+class sphere: public hittable{
+    public:
+    sphere(const point3& c , double r) : center(c) , radius(std::fmax(0,r)) {}
+    bool hit(const ray&r , double t_min , double t_max , hit_record& rec){
+        vec3 oc = center - r.get_origin();
+        auto h = dot(r.get_direction(),oc);
+        auto a = dot(r.get_direction(),r.get_direction());
+        auto c = dot(oc,oc) - radius * radius ;
+    
+        auto discriminant = h*h - a*c;
+        if(discriminant < 0 )
+            return false ;
+        auto root = h - sqrt(discriminant)/ ( a);
+        if (root < t_min || root > t_max)
+            {
+                root = h + sqrt(discriminant)/a ;
+                if (root < t_min || root > t_max)
+                    return false ;
+            }
+            rec.t = root ;
+            rec.p = r.at(root);
+            vec3 outward_normal = rec.p - center;
+            /* Convention, normals are always opposite to ray direction */
+            rec.set_front_face(r,outward_normal);
+            return true ;
+    }
+    private:
+    point3 center ;
+    double radius ;
+};
+#endif // SPHERE_H
