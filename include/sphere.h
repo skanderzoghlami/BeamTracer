db9a144cb7d0ea1960e7b62693b56655fb04794f
unknown <skander.zoghlami@etu.univ-lyon1.fr>
2025-03-18 11:03:46 +0100

    Fix: Solve the problem where the surface map wasn't looking correctly, turns out the problem was because of not normalizing the normal direction by dividing it by the radius

--- include/sphere.h
+++ include/sphere.h
@@ -10,14 +10,14 @@ class sphere: public hittable{
     sphere(const point3& c , double r) : center(c) , radius(std::fmax(0,r)) {}
     bool hit(const ray&r , double t_min , double t_max , hit_record& rec) const override{
         vec3 oc = center - r.get_origin();
-        auto h = dot(r.get_direction(),oc);
-        auto a = dot(r.get_direction(),r.get_direction());
-        auto c = dot(oc,oc) - radius * radius ;
+        auto a = r.get_direction().length_squared();
+        auto h = dot(r.get_direction(), oc);
+        auto c = oc.length_squared() - radius*radius;
     
         auto discriminant = h*h - a*c;
         if(discriminant < 0 )
             return false ;
-        auto root = h - sqrt(discriminant)/ ( a);
+        auto root = (h - sqrt(discriminant) )/ ( a);
         if (root < t_min || root > t_max)
             {
                 root = h + sqrt(discriminant)/a ;
@@ -26,7 +26,7 @@ class sphere: public hittable{
             }
             rec.t = root ;
             rec.p = r.at(root);
-            vec3 outward_normal = rec.p - center;
+            vec3 outward_normal = (rec.p - center)/radius;
             /* Convention, normals are always opposite to ray direction */
             rec.set_front_face(r,outward_normal);
             return true ;
