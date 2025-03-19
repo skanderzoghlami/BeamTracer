c1d5cf4b0b26eb23b01afbf545934dc50270f4ad
unknown <skander.zoghlami@etu.univ-lyon1.fr>
2025-03-19 14:18:52 +0100

    Add the Lamnbertian reflection

--- include/camera.h
+++ include/camera.h
@@ -65,7 +65,9 @@ color ray_color(const ray& r , int depth, const hittable& world ){
     if(depth <= 0)
         return color(0,0,0);
     if(world.hit(r,interval(0.001,infinity),rec)){
-        vec3 direction = random_on_hemisphere(rec.normal);
+        // vec3 direction = random_on_hemisphere(rec.normal); // Diffuse on hemisphere
+        // Lambertian Reflection
+        vec3 direction = random_unit_vector() + rec.normal;
         return 0.5 * ray_color(ray(rec.p,direction),depth-1,world);
     }
     vec3 unit_direction = unit_vector(r.get_direction());
