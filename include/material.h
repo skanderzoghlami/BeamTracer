3da0d686b0b2ad47e32332505888a4750de27d95
unknown <skander.zoghlami@etu.univ-lyon1.fr>
2025-03-25 16:31:33 +0100

    Feature: Add fuzziness

--- include/material.h
+++ include/material.h
@@ -34,19 +34,21 @@ private:
 class metal : public material
 {
 public:
-    metal(const color &albedo) : albedo(albedo) {}
+    metal(const color &albedo , double fuzz) : albedo(albedo) , fuzz(fuzz < 1 ? fuzz : 1) {}
 
     bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered)
         const override
     {
         vec3 reflected = reflect(r_in.get_direction(), rec.normal);
+        reflected = unit_vector(reflected)  + (fuzz * random_unit_vector());
         scattered = ray(rec.p, reflected);
         attenuation = albedo;
-        return true;
+        return (dot(scattered.get_direction(), rec.normal) > 0);
     }
 
 private:
     color albedo;
+    double fuzz;
 };
 
 #endif // MATERIAL_H
